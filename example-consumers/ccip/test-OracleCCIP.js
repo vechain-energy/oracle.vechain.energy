65c534ba39870bc7a22cbb9b1d042807d7fe7b21
Mario Micklisch <mario@favo.org>
2023-12-14 13:01:14 +0100

    Improve CCIP example snippet

--- example-consumers/ccip/test-OracleCCIP.js
+++ example-consumers/ccip/test-OracleCCIP.js
@@ -8,33 +8,56 @@ if (!contractAddress) {
     process.exit(1)
 }
 
-const ccipContract = new Interface([
+const ccipContractInterface = new Interface([
     'function usdPriceVet()',
     'error OffchainLookup(address sender, string[] urls, bytes callData, bytes4 callbackFunction, bytes extraData)',
-    'function usdPriceVetWithProof(bytes calldata response, bytes calldata extraData) returns (uint128 value, uint128 updatedAt, bytes feedId)'
+    'function usdPriceVetWithProof(bytes calldata response, bytes calldata extraData) returns (uint128 value, uint128 updatedAt, bytes32 feedId)'
 ])
 
+const oracleAddress = '0x12E3582D7ca22234f39D2A7BE12C98ea9c077E25'
+
 async function main() {
+
+    console.log('Asking Oracle Contract\n')
+    const resultOracle = await connexCall({
+        address: oracleAddress,
+        abi: { "inputs": [{ "internalType": "bytes32", "name": "id", "type": "bytes32" }], "name": "getLatestValue", "outputs": [{ "internalType": "uint128", "name": "value", "type": "uint128" }, { "internalType": "uint128", "name": "updatedAt", "type": "uint128" }], "stateMutability": "view", "type": "function" },
+        args: [ethers.encodeBytes32String('vet-usd')]
+    })
+    console.log('Value', ethers.formatUnits(resultOracle.decoded.value, 12))
+    console.log('Updated At', (new Date(Number(resultOracle.decoded.updatedAt) * 1000).toISOString()))
+
+    console.log('\n\n')
+
+    console.log('Asking CCIP Contract\n')
+    const resultOffChain = await connexCall({
+        address: contractAddress,
+        abi: { "inputs": [], "name": "usdPriceVet", "outputs": [], "stateMutability": "view", "type": "function" },
+        args: []
+    })
+    console.log('Value', ethers.formatUnits(resultOffChain.decoded.value, 12))
+    console.log('Updated At', (new Date(Number(resultOffChain.decoded.updatedAt) * 1000).toISOString()))
+    console.log('Feed Id', ethers.decodeBytes32String(resultOffChain.decoded.feedId))
+
+}
+
+async function connexCall({ address, abi, args }) {
     const driver = await Driver.connect(new SimpleNet('https://node-testnet.vechain.energy'))
     const connex = new Framework(driver)
 
     const result = await connex.thor
-        .account(contractAddress)
-        .method({
-            "inputs": [],
-            "name": "usdPriceVet",
-            "outputs": [],
-            "stateMutability": "view",
-            "type": "function"
-        })
-        .call()
+        .account(address)
+        .method(abi)
+        .call(...args)
 
+    if (!result.reverted) {
+        return result
+    }
     if (result.reverted) {
-        const error = ccipContract.parseError(result.data)
+        const error = ccipContractInterface.parseError(result.data)
 
         if (error.name === 'OffchainLookup') {
-
-            console.log('OffchainLookup detected')
+            // console.log('OffchainLookup detected')
             const backendArgs = {
                 sender: ethers.hexlify(error.args[0]),
                 urls: error.args.urls,
@@ -45,13 +68,13 @@ async function main() {
 
             for (const url of error.args.urls) {
                 const response = await (async() => {
-                    console.log('fetching response from', url)
+                    // console.log('fetching response from', url)
                     const queryUrl = url.replace(/\{([^}]*)\}/g, (match, p1) => backendArgs[p1]);
                     if (url.includes('{data}')) {
-                        console.log('GET', `${url}`.replace('{sender}', backendArgs.sender).replace('{data}', backendArgs.data))
+                        // console.log('GET', `${url}`.replace('{sender}', backendArgs.sender).replace('{data}', backendArgs.data))
                         return await fetch(queryUrl)
                     } else {
-                        console.log('POST', queryUrl, backendArgs)
+                        // console.log('POST', queryUrl, backendArgs)
                         return await fetch(`${queryUrl}`, {
                             method: 'POST',
                             headers: {
@@ -64,48 +87,40 @@ async function main() {
                 })()
 
                 const { data } = await response.json()
-                const result = await connex.thor
-                    .account(contractAddress)
-                    .method({
-                        "inputs": [{
-                                "internalType": "bytes",
-                                "name": "response",
-                                "type": "bytes"
-                            },
-                            {
-                                "internalType": "bytes",
-                                "name": "extraData",
-                                "type": "bytes"
-                            }
-                        ],
-                        "name": "usdPriceVetWithProof",
-                        "outputs": [{
-                                "internalType": "uint128",
-                                "name": "value",
-                                "type": "uint128"
-                            },
-                            {
-                                "internalType": "uint128",
-                                "name": "updatedAt",
-                                "type": "uint128"
-                            },
-                            {
-                                "internalType": "bytes32",
-                                "name": "feedId",
-                                "type": "bytes32"
-                            }
-                        ],
-                        "stateMutability": "view",
-                        "type": "function"
+
+                // callbackFunction + args()
+                const contractCall = ethers.solidityPacked(
+                    ['bytes4', 'bytes'], [
+                        // callbackFunction(bytes, bytes)
+                        backendArgs.callbackFunction, ethers.AbiCoder.defaultAbiCoder().encode(
+                            ['bytes', 'bytes'], [data, backendArgs.extraData]
+                        )
+                    ]
+                );
+
+                const [result] = await (await fetch('https://node-testnet.vechain.energy/accounts/*', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        clauses: [{
+                            to: backendArgs.sender,
+                            data: contractCall
+                        }]
                     })
-                    .call(data, error.args.extraData)
+                })).json()
 
                 if (result.reverted) {
                     console.error(result)
                     throw new Error(result.revertReason)
                 }
 
-                console.log('Current data is', ethers.formatUnits(result.decoded.value, 12), 'updated at', (new Date(Number(result.decoded.updatedAt) * 1000).toISOString()), 'for feed', ethers.decodeBytes32String(result.decoded.feedId))
+                const decoded = ccipContractInterface.decodeFunctionResult(backendArgs.callbackFunction, result.data)
+                return {
+                    ...result,
+                    decoded
+                }
             }
         }
     }
