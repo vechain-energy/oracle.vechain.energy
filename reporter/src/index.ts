705e049129179f05a26bfac070cac2ddac5eb03b
Mario Micklisch <mario@favo.org>
2023-12-19 19:28:28 +0100

    Instruct crawlers not to index

--- reporter/src/index.ts
+++ reporter/src/index.ts
@@ -18,6 +18,11 @@ export default {
 		}
 
 		const url = new URL(request.url)
+
+		if (url.pathname == "/robots.txt") {
+			return new Response("User-agent: *\nDisallow: /", { headers: { "Content-Type": "text/plain" } });
+		}
+
 		const idFromUrl = url.pathname.slice(1).split('/')[0]
 		const doId = env.ValueReporter.idFromName(idFromUrl);
 		const stub = env.ValueReporter.get(doId)
