3a0383ea64bcd62de3d84d09e4e7cf6ad449c9ba
Mario Micklisch <mario@favo.org>
2023-12-19 19:37:45 +0100

    Improve UI overview

--- statuspage/src/Feed/Status.tsx
+++ statuspage/src/Feed/Status.tsx
@@ -5,7 +5,7 @@ import type { Status } from '../types'
 import LoadingIndicator from './LoadingIndicator';
 import FeedSheet from './Sheet';
 
-const UPDATE_INTERVAL_SECONDS = 30
+const UPDATE_INTERVAL_SECONDS = 60
 export default function FeedStatus({ feedId }: { feedId: string }): React.ReactElement {
     const { data, isLoading } = useQuery<Status | { message: string }>({
         queryKey: [feedId],
